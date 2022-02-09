25609357fdad9b6861d8645b73dcb78bc4ac92e1
CatLuck2 <freeaddress834@gmail.com>
2022-02-09 16:17:28 +0900

    5つの値を足し算できるようにした

--- Kadai1/Controller/ViewController.swift
+++ Kadai1/Controller/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    
     @IBOutlet weak private var inputNumTextField1: UITextField!
     @IBOutlet weak private var inputNumTextField2: UITextField!
     @IBOutlet weak private var inputNumTextField3: UITextField!
@@ -17,12 +16,30 @@ final class ViewController: UIViewController {
     @IBOutlet weak private var inputNumTextField5: UITextField!
     @IBOutlet weak private var resultOfAdditionLabel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func calculateAdditionButton(_ sender: UIButton) {
+        var result: Int = 0
+        guard let inputText1 = inputNumTextField1.text,
+            let inputText2 = inputNumTextField2.text,
+            let inputText3 = inputNumTextField3.text,
+            let inputText4 = inputNumTextField4.text,
+            let inputText5 = inputNumTextField5.text
+        else {
+            return
+        }
+        result += Int(inputText1) ?? 0
+        result += Int(inputText2) ?? 0
+        result += Int(inputText3) ?? 0
+        result += Int(inputText4) ?? 0
+        result += Int(inputText5) ?? 0
+        setResultOfAdditionLabel(result: result)
+    }
+    
+    private func setResultOfAdditionLabel(result: Int) {
+        resultOfAdditionLabel.text = "\(result)"
     }
     
 }
