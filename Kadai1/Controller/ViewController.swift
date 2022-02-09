a2c9e5217ee19e06abbffff96641540996285a94
CatLuck2 <freeaddress834@gmail.com>
2022-02-09 10:30:09 +0900

    UI部品を追加、ViewControllerに接続、classにfinalを付与、不要なコメントを削除

---
+++ Kadai1/Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Kadai1
+//
+//  Created by Nekokichi on 2022/02/06.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak private var inputNumTextField1: UITextField!
+    @IBOutlet weak private var inputNumTextField2: UITextField!
+    @IBOutlet weak private var inputNumTextField3: UITextField!
+    @IBOutlet weak private var inputNumTextField4: UITextField!
+    @IBOutlet weak private var inputNumTextField5: UITextField!
+    @IBOutlet weak private var resultOfAdditionLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func calculateAdditionButton(_ sender: UIButton) {
+    }
+    
+}
+
