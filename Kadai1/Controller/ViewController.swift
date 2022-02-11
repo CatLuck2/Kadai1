36cd823423187bb7813a55fc1cc9b3a61cc81779
akio0911 <akio0911@gmail.com>
2022-02-11 13:16:47 +0900

    feedback

--- Kadai1/Controller/ViewController.swift
+++ Kadai1/Controller/ViewController.swift
@@ -16,25 +16,21 @@ final class ViewController: UIViewController {
     @IBOutlet weak private var inputNumTextField5: UITextField!
     @IBOutlet weak private var resultOfAdditionLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBAction func calculateAdditionButton(_ sender: UIButton) {
-        var calculateAdditionModel = CalculateModel.Addition()
+        let calculateAdditionModel: CalculateModelProtocol = CalculateModel()
         
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField1.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField2.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField3.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField4.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField5.text ?? "") ?? 0)
+        let result = calculateAdditionModel.calculate(values: [
+            Int(inputNumTextField1.text ?? "") ?? 0,
+            Int(inputNumTextField2.text ?? "") ?? 0,
+            Int(inputNumTextField3.text ?? "") ?? 0,
+            Int(inputNumTextField4.text ?? "") ?? 0,
+            Int(inputNumTextField5.text ?? "") ?? 0,
+        ])
         
-        setResultOfAdditionLabel(result: calculateAdditionModel.getResult())
+        setResultOfAdditionLabel(result: result)
     }
     
     private func setResultOfAdditionLabel(result: Int) {
         resultOfAdditionLabel.text = "\(result)"
     }
-    
 }
-
