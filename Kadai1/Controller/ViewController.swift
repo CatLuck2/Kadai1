2cf59a3e162145b1a71b1838d17aca5ebe071d00
CatLuck2 <freeaddress834@gmail.com>
2022-02-14 07:48:23 +0900

    フィードバック通りに修正、それに対するコメントを追加

--- Kadai1/Controller/ViewController.swift
+++ Kadai1/Controller/ViewController.swift
@@ -15,21 +15,46 @@ final class ViewController: UIViewController {
     @IBOutlet weak private var inputNumTextField4: UITextField!
     @IBOutlet weak private var inputNumTextField5: UITextField!
     @IBOutlet weak private var resultOfAdditionLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
+    /*
+     不要なメソッドは要らない
+     ・未使用なので、存在する必要がない
+     ・消せば、コード量が減り、読みやすくなる
+     */
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+    
     @IBAction func calculateAdditionButton(_ sender: UIButton) {
-        var calculateAdditionModel = CalculateModel.Addition()
+        /*
+         ・プロトコルを型付け
+         ・プロトコルに準拠した構造体に対応
+         */
+        let calculateAdditionModel: CalculateModelProtocol = CalculateModel()
         
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField1.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField2.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField3.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField4.text ?? "") ?? 0)
-        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField5.text ?? "") ?? 0)
+        /*
+         ・呼び出しの共通部分を省略
+         ・後から追加する際、要素のみを加えればいい
+         */
+        let result = calculateAdditionModel.calculate(values: [
+             Int(inputNumTextField1.text ?? "") ?? 0,
+             Int(inputNumTextField2.text ?? "") ?? 0,
+             Int(inputNumTextField3.text ?? "") ?? 0,
+             Int(inputNumTextField4.text ?? "") ?? 0,
+             Int(inputNumTextField5.text ?? "") ?? 0,
+         ])
+
+        setResultOfAdditionLabel(result: result)
         
-        setResultOfAdditionLabel(result: calculateAdditionModel.getResult())
+//        var calculateAdditionModel = CalculateModel.Addition()
+        
+//        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField1.text ?? "") ?? 0)
+//        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField2.text ?? "") ?? 0)
+//        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField3.text ?? "") ?? 0)
+//        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField4.text ?? "") ?? 0)
+//        calculateAdditionModel.calculate(inputValue: Int(inputNumTextField5.text ?? "") ?? 0)
+        
+//        setResultOfAdditionLabel(result: calculateAdditionModel.getResult())
     }
     
     private func setResultOfAdditionLabel(result: Int) {
--- Kadai1/Model/CalculateModel.swift
+++ Kadai1/Model/CalculateModel.swift
@@ -7,20 +7,57 @@
 
 import UIKit
 
-protocol CalculateProtocol {
-    var result: Int { get }
-    mutating func calculate(inputValue: Int)
-    func getResult() -> Int
+/*
+ 変更後
+ ・プロトコルの変数result、関数getResultが無くなった
+ ・構造体Addtionが無くなった
+ ・関数calculateに計算と取得を実装
+ →より抽象的なプロトコル
+ →calculateメソッドを使用するだけでいい
+ 
+ なぜプロトコルを使ったのか？
+ ・足し算以外の計算を実装するのを柔軟にするため
+ ・実装すべき処理を強制できるので、実装忘れを防げる
+ */
+
+protocol CalculateModelProtocol {
+     func calculate(values: [Int]) -> Int
+ }
+
+/*
+ CalculateModelProtoclに準拠する型なら、
+ 共通の処理を統一できる
+ */
+//struct CalculateControlModel {
+//    func example(model: CalculateModelProtocol) {
+//    }
+//}
+
+struct CalculateModel: CalculateModelProtocol {
+     func calculate(values: [Int]) -> Int {
+         var result: Int = 0
+          for value in values {
+              result += value
+          }
+          return result
+     }
 }
 
-final class CalculateModel {
-    struct Addition: CalculateProtocol {
-        var result: Int = 0
-        mutating func calculate(inputValue: Int) {
-            result += inputValue
-        }
-        func getResult() -> Int {
-            result
-        }
-    }
-}
+
+//protocol CalculateProtocol {
+//    var result: Int { get }
+//    mutating func calculate(inputValue: Int)
+//    func getResult() -> Int
+//}
+
+//final class CalculateModel {
+//    struct Addition: CalculateProtocol {
+//        var result: Int = 0
+//        mutating func calculate(inputValue: Int) {
+//            result += inputValue
+//        }
+//        func getResult() -> Int {
+//            result
+//        }
+//    }
+//}
